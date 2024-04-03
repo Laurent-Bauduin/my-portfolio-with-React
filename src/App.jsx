72125d715cc696a988b10d6f2ee72b4e5bc41e21
Laurent-Bauduin <laurent.bauduin@outlook.com>
2024-04-03 10:17:29 +0200

    importing CSS from my 1st portfolio JS vanilla + creating component Header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Header from "./components/Header.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <main>
+        <section id="section-photo">
+          <img src="src/assets/Laurent.jpg" id="photo" />
+          <p>Valenciennes - France</p>
+        </section>
+        <section className="summary">
+          <h2>En bref...</h2>
+          <p>
+            Je suis étudiant en Développement Web à la Wild Code School, où je
+            suis un cursus de 5 mois orienté JavaScript (front-end).
+          </p>
+          <a href="https://github.com/Laurent-Bauduin">
+            <img
+              src="src/assets/github_git_icon_145985.svg"
+              alt="GitHub logo"
+              id="GH"
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/laurent-bauduin-868601250/">
+            <img src="src/assets/linked-in.png" alt="LinkedIn logo" id="LI" />
+          </a>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <>
+      <header>
+        <h1>Laurent Bauduin</h1>
+        <button className="theme">Nuit/Jour</button>
+        <p>Full-stack Web Developer</p>
+      </header>
+    </>
+  );
+}
+
+export default Header;
